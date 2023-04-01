61636eea6ada41e591a8754e10ec5c46b1070734
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-04-01 23:19:15 +0200

    reduction du JS avec une fonction et une boucle

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.pageY + "px";
+//   cursor.style.left = e.pageX + "px";
+//   mouse1.style.top = e.pageY + "px";
+//   mouse1.style.left = e.pageX + "px";
+//   mouse2.style.top = e.pageY + "px";
+//   mouse2.style.left = e.pageX + "px";
+// });
 
-// const mousecursor = document.querySelector(".mousecursor");
-// const mouseCircle1 = document.querySelector(".mousecircle1");
-// const mouseCircle2 = document.querySelector(".mousecircle2");
+// reduction de ce code grace à une fonction et une bouckle forEach :
 
-// je vais mettre des id aux balises html des cercles, donc plus besoin de querySelector car JS reconnais nativement les id !
+const mouses = document.querySelectorAll(".mouse");
 
 window.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.pageY + "px";
-  cursor.style.left = e.pageX + "px";
-  mouse1.style.top = e.pageY + "px";
-  mouse1.style.left = e.pageX + "px";
-  mouse2.style.top = e.pageY + "px";
-  mouse2.style.left = e.pageX + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+  });
 });
-
-// S'assurer que les liens sont clickables
-
-// --> dans le CSS, je mets un z-index fort sur les liens de la nav
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
-
-// --> dans le CSS
